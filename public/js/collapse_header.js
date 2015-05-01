11cbc1370940c835b2425233d56030299c94eade
ThomWright <github@thomwright.co.uk>
2015-05-01 12:25:02 +0100

    Improve header collapse js

--- public/js/collapse_header.js
+++ public/js/collapse_header.js
@@ -1,26 +1,33 @@
-$(document).ready(function () {
-  var outer = $('#affix-outer'),
-      collapse = $('#affix-collapse'),
-      sticky = $('#affix-sticky'),
-      wrapper = $('#affix-wrapper'),
-      setWrapperHeight = function() {
-        wrapper.css('min-height', sticky.height());
-      },
-      getOffset = function() {
-        return collapse.outerHeight(true/*include margin*/);
-      };
+if (typeof jQuery === 'undefined') {
+  throw new Error('Custom JS requires jQuery...');
+}
 
-  setWrapperHeight();
++function ($) {
+  'use strict';
+  $(document).ready(function () {
+    var outer = $('#affix-outer'),
+        collapse = $('#affix-collapse'),
+        sticky = $('#affix-sticky'),
+        wrapper = $('#affix-wrapper'),
+        setWrapperHeight = function() {
+          wrapper.css('min-height', sticky.height());
+        },
+        getOffset = function() {
+          return collapse.outerHeight(true/*include margin*/);
+        };
 
-  outer.affix({
-    offset: {
-      top: getOffset()
-    }
-  });
-
-  $(window).on("resize", function(){
     setWrapperHeight();
-    outer.data('bs.affix').options.offset = getOffset();
+
+    outer.affix({
+      offset: {
+        top: getOffset()
+      }
+    });
+
+    $(window).on("resize", function(){
+      setWrapperHeight();
+      outer.data('bs.affix').options.offset = getOffset();
+    });
   });
-});
+}(jQuery);
 
